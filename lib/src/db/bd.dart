7d546166e073648a3d0c2b2b2a5762d88dd52a76
Luara <luara@MacBook-Pro-de-Luara.local>
2025-05-23 15:15:36 -0300

    Revert "teste"
    This reverts commit a23debd21a0748a78a71fe05c0e9ad498425b57c.
    
    git add .
    git commit -m "teste"
    git push origin F_20250523

--- lib/src/db/bd.dart
+++ lib/src/db/bd.dart
@@ -1,12 +1,4 @@
  class DB{
 
-  static const String dbName = 'my_database.db';
-  static const int dbVersion = 1;
 
-  static const String tableName = 'my_table';
-  static const String columnId = 'id';
-  static const String columnName = 'name';
-  static const String columnAge = 'age';
-
-  // essas sao as alteracoes do meu commit
 }
